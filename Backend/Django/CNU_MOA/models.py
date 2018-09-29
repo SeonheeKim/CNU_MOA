b52d02cd9c856dd6119a58e613ab43658b791f04
SeonheeKim <dnwdjqldb@cnu.ac.kr>
2018-09-29 19:04:22 +0900

    db table create

--- Backend/Django/CNU_MOA/models.py
+++ Backend/Django/CNU_MOA/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class CrawlingData(models.Model):
+    depart = models.IntegerField(default=-1)
+    board = models.IntegerField(default=0)
+    title = models.CharField(max_length=90)
+    writer = models.CharField(max_length=20)
+    date = models.CharField(max_length=20)
+    link = models.CharField(null=True, max_length=300)
+    views = models.IntegerField(default=0)
--- Backend/Django/Django/settings.py
+++ Backend/Django/Django/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = '*'
 # Application definition
 
 INSTALLED_APPS = [
-    'CNU_MOA',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'CNU_MOA',
 ]
 
 MIDDLEWARE = [
