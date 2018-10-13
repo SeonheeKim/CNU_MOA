a8d0d836ceba08b0bf6749bee9554240df6a688f
SeonheeKim <dnwdjqldb@cnu.ac.kr>
2018-10-13 21:10:56 +0900

    filter add

--- Backend/Django/CNU_MOA/admin.py
+++ Backend/Django/CNU_MOA/admin.py
@@ -1,5 +1,11 @@
+# CNU_MOA/admin.py
 from django.contrib import admin
+from .models import CrawlingData
+
+
+class CrawlingAdmin(admin.ModelAdmin):
+    list_display = ('depart', 'board', 'title', 'writer', 'link', 'views', 'year', 'month', 'day')
+
 
 # Register your models here.
-
 admin.site.register(CrawlingData)
--- Backend/Django/CNU_MOA/models.py
+++ Backend/Django/CNU_MOA/models.py
@@ -1,6 +1,5 @@
+# CNU_MOA/models.py
 from django.db import models
-
-# Create your models here.
 
 
 class CrawlingData(models.Model):
@@ -8,6 +7,8 @@ class CrawlingData(models.Model):
     board = models.IntegerField(default=0)
     title = models.CharField(max_length=90)
     writer = models.CharField(max_length=20)
-    date = models.CharField(max_length=20)
     link = models.CharField(null=True, max_length=300)
     views = models.IntegerField(default=0)
+    year = models.IntegerField(default=2000)
+    month = models.IntegerField(default=1)
+    day = models.IntegerField(default=1)
